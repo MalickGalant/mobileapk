6d9b3d7e680c5afb8d70e317cade70578f59fa21
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-02-05 13:46:04 +0200

    Update GenerateUserCredentials.java

--- GenerateUserCredentials.java
+++ GenerateUserCredentials.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.github.MalickGalant.application.authentication;
+package io.github.malickgalant.application;
 
 import com.beust.jcommander.Parameter;
 import com.google.ads.googleads.examples.utils.ArgumentNames;
