adf4812d07046c1109b95b9858d6b6982a5f0356
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-02-05 22:17:26 +0200

    Update MainComponent.jsx

--- demo/MainComponent.jsx
+++ demo/MainComponent.jsx
@@ -5,35 +5,36 @@ const MainComponent = React.createClass({
   render: function() {
     return (
       <div>
-        <h1> react-adsense: adsense for react </h1>
+        <h1>Mobile React App</h1>
         <p>
-          Project <a href="https://github.com/hustcc/react-adsense">react-adsense</a> is a React-component for Google AdSense / Baidu advertisement.
+          Project <a href="https://github.com/malickgalant/mobileapk">React-Adsense</a> is a React-component for Google AdSense / Baidu advertisement.
         </p>
 
-        <h2> 1. Install </h2>
+        <h2>Install </h2>
         <pre className="strong"> npm install react-adsense </pre>
 
-        <p> Then import it. </p>
+        <p>Then import it. </p>
         <pre>{"//import library use script tag.\n\nimport AdSense from 'react-adsense';"}
         </pre>
 
-        <h2> 2. Usage </h2>
+        <h2>Ads Usage</h2>
         <pre>{"import AdSense from 'react-adsense';\n\n<AdSense.Google\n\tclient='ca-pub-7292810486004926'\n\tslot='7806394673'\n/>"}
 
         </pre>
 
-        <h2> 3. default props </h2>
+        <h2> Default props </h2>
         <AdSense.Google client='ca-pub-7292810486004926'
                         slot='7806394673' />
 
-        <h2> 4. in-article layout </h2>
+        <h2>Article's Trending</h2>
+
         <AdSense.Google
             client='ca-pub-7292810486004926'
             slot='7806394673'
             layout='in-article'
         />
 
-        <h2> 5. full-config Demo </h2>
+        <h2>Full Demo</h2>
         <AdSense.Google
             client='ca-pub-7292810486004926'
             slot='7806394673'
