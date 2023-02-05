47e6b6529af47cf134561d004c8c9c1f0615883a
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-02-05 16:38:12 +0200

    Update ShoppingAd.java

--- ShoppingAd.java
+++ ShoppingAd.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package io.github.malickgalant.application;
+package io.github.malickgalant.google-ads;
 
 import static com.google.ads.googleads.examples.utils.CodeSampleHelper.getPrintableDateTime;
 
